7355ef4c83849056b6cf1070c271dd6ddf05b442
Stephen Hulme <smhulme@hotmail.com>
2016-02-16 20:35:44 +0000

    first change to drop in call for customer CLI call

---
+++ src/test/java/org/test/service/CLI/CustomerCLIByDayProcessorTest.java
@@ -0,0 +1,19 @@
+package org.test.service.CLI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomerCLIByDayProcessorTest {
+
+    @InjectMocks
+    private CustomerCLIByDayProcessor customerCLIByDayProcessor;
+
+    @Test
+    public void testProcessCallInformation() {
+
+    }
+
+}
